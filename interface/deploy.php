e0d87b2bdb7fe2246cbaa149c57deb548a8420e4
Pavel Emelyanov <punkbeast553@gmail.com>
2017-12-27 21:51:19 +0300

    COME ON WORK PLZ

--- interface/deploy.php
+++ interface/deploy.php
@@ -2,17 +2,17 @@
 	// Anti-XSS
 	header("Content-type: text/plain");
 	//if(empty($_POST['payload'])) die('wat');
-	
+
 	//fetch headers
-	$headers = []; 
-	foreach ($_SERVER as $name => $value) 
-	{ 
-		if (substr($name, 0, 5) == 'HTTP_') 
-		{ 
-			$headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value; 
-		} 
+	$headers = [];
+	foreach ($_SERVER as $name => $value)
+	{
+		if (substr($name, 0, 5) == 'HTTP_')
+		{
+			$headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+		}
 	}
-	 
+
 	//check auth
 	$secret = 'f8b5fd97df670h5sdfh4dfs35df6g3';
 	$hubSignature = $headers['X-Hub-Signature'];
@@ -22,6 +22,6 @@
 	if ($hash !== $payloadHash) die('no');
 
 	//sync
-	$out = shell_exec('./deploy.sh');
+	$out = shell_exec('cd .. && git reset --hard HEAD && git pull https://3cae39f6cc90ba28d1acf9ccc44ad8622882d213@github.com/nakiner/tbot.git master');
 	echo $out;
 ?>
